e91f11bc78405532f870c66beb7a683ff0c58d26
Dhanuj R <83819952+Dhanuj04@users.noreply.github.com>
2023-10-03 23:00:41 +0530

    Update Main.java

--- Java_Assignments/Day 4_Offline/src/assignment5/Main.java
+++ Java_Assignments/Day 4_Offline/src/assignment5/Main.java
@@ -2,7 +2,7 @@ package com.pack.assignment5;
 
 public class Main {
     public static void main(String[] args) {
-        DayScholar dayScholar = new DayScholar(5, 'D',"OOP", 16000,"Manu",1001, "NO 32/3 Vijaywada");
+        DayScholar dayScholar = new DayScholar(5, 'D',"OOP", 16000,"Dhanuj",1001, "NO 32/3 Chennai");
 
         System.out.println(dayScholar.calculateFees());
         System.out.println(dayScholar.getFees());
