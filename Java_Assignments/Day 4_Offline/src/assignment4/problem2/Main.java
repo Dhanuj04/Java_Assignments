2a7c70bfdea761a503232c9dfeb30218e7061417
Dhanuj R <83819952+Dhanuj04@users.noreply.github.com>
2023-10-03 23:10:55 +0530

    Update Main.java

--- Java_Assignments/Day 4_Offline/src/assignment4/problem2/Main.java
+++ Java_Assignments/Day 4_Offline/src/assignment4/problem2/Main.java
@@ -66,7 +66,7 @@ class DayScholar extends Student {
 
 class CourseReg {
 	public static void main(String args[]){ 
-		DayScholar dayscholar = new DayScholar (1001,'D',"Eugene",12000, 1000,"No32/68 Vijayanagar","00P"); 
+		DayScholar dayscholar = new DayScholar (1001,'D',"Dhanuj",12000, 1000,"No32/68 Gandhinagar","00P"); 
 		dayscholar.displayDetails(); 
 	}
 
