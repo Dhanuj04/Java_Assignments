31e2a3d3ca11f8aa9a56e964710e89c4b2c63083
Dhanuj R <83819952+Dhanuj04@users.noreply.github.com>
2023-10-03 23:08:37 +0530

    Update Main.java

--- Java_Assignments/Day 4_Offline/src/assignment3/Main.java
+++ Java_Assignments/Day 4_Offline/src/assignment3/Main.java
@@ -11,10 +11,10 @@ public class Main {
 		
 		
 		if(residentialStatus.equalsIgnoreCase("hostellite")) {
-			s = new HostelLite(1, 'm', "Rakshit", 3000);
+			s = new HostelLite(1, 'm', "Sandhya", 3000);
 			s.displayStudentDetails();
 		} else if(residentialStatus.equalsIgnoreCase("dayscholar")) {
-			s = new DayScholar(1, 'm', "Kartik", 0);
+			s = new DayScholar(1, 'm', "Dhanuj", 0);
 			s.displayStudentDetails();
 		} else {
 			System.out.println("Invalid Input");
